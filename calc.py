3eaff098d08abc51eb2475c1fa4cc73f2f4ba7a9
Pooja-S-2006 <poojasenthil@1521>
2026-04-15 20:52:47 +0530

    Added basic functions

---
+++ calc.py
@@ -0,0 +1,21 @@
+# math library functions
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
+
+def power(a, b):
+    return a ** b
+
+def modulus(a, b):
+    return a % b
